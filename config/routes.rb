25b214e969ce9c1243e3431e672df76e8b20eecd
Chris Bastian <peacethrubeats@gmail.com>
2015-02-02 11:31:25 -0800

    welcome controller deleted

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
-  get 'static/pages/help'
-  get 'static/pages/about'
-  get 'static/pages/contact'
+  root              'static_pages#home'
+  get 'help' =>     'static_pages#help'
+  get 'about' =>    'static_pages#about'
+  get 'contact' =>  'static_pages#contact'
 
   resources :users
 
