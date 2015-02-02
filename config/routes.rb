5bb793ac7c3dc2276f1bccc9cd0fbfc9659f51b2
Chris Bastian <peacethrubeats@gmail.com>
2015-02-02 10:26:51 -0800

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+  get 'static/pages/help'
+  get 'static/pages/about'
+  get 'static/pages/contact'
 
   resources :users
 
-  root 'welcome#index'
 end
