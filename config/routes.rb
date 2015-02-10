aec0ca0fa4cc84c0325df19179bce1ca7225bcfd
Chris Bastian <peacethrubeats@gmail.com>
2015-02-10 10:40:45 -0800

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  root              'static_pages#home'
-  get 'help' =>     'static_pages#help'
-  get 'about' =>    'static_pages#about'
-  get 'contact' =>  'static_pages#contact'
-  get 'signup' =>   'users#new'
 
-  get 'static_pages/home'
 
-  get 'static_pages/help'
+  root             'static_pages#home'
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'news'    => 'static_pages#news'
 
-  resources :users
+  get 'signup'  => 'users#new'
+
+  # resources :users
 
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,4 +11,16 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Relationship 2.0"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Relationship 2.0"
+  end
+
 end
