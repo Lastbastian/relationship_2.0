3858750c067ddb531751cbfdb27b6440f9f557e1
Chris Bastian <peacethrubeats@gmail.com>
2015-02-13 18:35:42 -0800

    questionnaires

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -1,5 +1,12 @@
 class QuestionnairesController < ApplicationController
-  def show
-    @questionnaire = Questionnaire.find(params[:questionnaire_id])
+
+  def index
+    @questionnaire = Questionnaire.all
   end
+
+  def show
+     @questionnaire = Questionnaire.find(params[:id])
+     @questions = @questionnaire.questions
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,6 @@ Rails.application.routes.draw do
 
   resources :questionnaires
 
+  resources :questions
+
 end
